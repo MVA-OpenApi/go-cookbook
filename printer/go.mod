f4cf97ad5072efb2f7407357bde4aeae58b80df6
Christian <christian.szramek@web.de>
2022-05-07 18:09:38 +0200

    progress on printer printer

--- printer/go.mod
+++ printer/go.mod
@@ -2,4 +2,7 @@ module printer
 
 go 1.13
 
-require github.com/spf13/cobra v1.4.0
+require (
+	github.com/dave/jennifer v1.5.0 // indirect
+	github.com/spf13/cobra v1.4.0
+)
--- printer/src/util.go
+++ printer/src/util.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	. "github.com/dave/jennifer/jen"
 )
 
 func CheckIfFileExists(path string) bool {
@@ -24,4 +25,24 @@ type Names struct {
 
 type Cities struct {
 	Cities []string `json:"cities"`
+}
+
+// @input: Array with strings, that have to be printed
+// @ouput: ./ouput/main.go file that can print the strings (either cities or names)
+func GenerateGoFile(strings []string, output_path string) {
+	f := NewFile("main")
+	f.Func().Id("main").Params().Block(
+		Qual("fmt", "Println").Call(Lit(output_path)), // dont know why double slashes gets generated
+		Id("strings").Op(":=").Index().String().Values(LitFunc(func() interface{} {
+			return `"jo", "jo2"`
+		})),
+		For(
+			Id("i").Op(":=").Lit(0),
+			Id("i").Op("<").Lit(len(strings)),
+			Id("i").Op("++"),	
+		).Block(
+			Qual("fmt", "Println").Call(Id("strings").Index(Id("i"))),	
+		),
+	)
+	fmt.Printf("%#v", f)
 }
