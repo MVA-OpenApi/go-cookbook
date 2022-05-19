a9d0982e4e334c52185113a38c8b1625ae1941f4
AyyKamp <arda513@hotmail.de>
2022-05-19 18:50:27 +0200

    fixed formatting

--- logging/log.go
+++ logging/log.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-		"errors"
-		"os"
-    "github.com/rs/zerolog"
-    "github.com/rs/zerolog/log"
+	"errors"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	  // set time format 
-    zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	// set time format
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-		// use pretty logging
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	// use pretty logging
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-		// enable deubgging
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	// enable deubgging
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 
-    log.Debug().Msg("This message appears only when log level set to Debug")
-    log.Warn().Msg("This message appears only when log level set to Warn or above")
-    log.Info().Msg("This message appears when log level set to Info or above")
+	log.Debug().Msg("This message appears only when log level set to Debug")
+	log.Warn().Msg("This message appears only when log level set to Warn or above")
+	log.Info().Msg("This message appears when log level set to Info or above")
 
-		// logging errors
-		err := errors.New("seems we have an error here")
-		log.Error().Err(err).Msg("")
+	// logging errors
+	err := errors.New("seems we have an error here")
+	log.Error().Err(err).Msg("")
 
 }
